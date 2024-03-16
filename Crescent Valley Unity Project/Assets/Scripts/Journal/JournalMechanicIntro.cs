419c88e086ac4820f9c0708e6608ede557df91ae
Charleychar <Charleychar@outlook.com>
2024-03-16 12:22:47 +0000

    journal tutorial works

--- Crescent Valley Unity Project/Assets/Scripts/Journal/JournalMechanicIntro.cs
+++ Crescent Valley Unity Project/Assets/Scripts/Journal/JournalMechanicIntro.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Video;
+using TMPro;
 
 public class JournalMechanicIntro : MonoBehaviour
 {
     [SerializeField] Canvas journalTutorialCanvas;
     [SerializeField] Canvas journalUI;
     [SerializeField] string PlayerTag;
-    [SerializeField] GameObject line1;
-    [SerializeField] GameObject line2;
+    [SerializeField] TextMeshProUGUI line1;
+    [SerializeField] TextMeshProUGUI line2;
+    [SerializeField] Cutscenes cutscenePlayer;
+    [SerializeField] VideoClip idaFlower2;
     //[SerializeField] GameObject player;
 
     GameObject journal;
@@ -22,12 +26,24 @@ public class JournalMechanicIntro : MonoBehaviour
         journalTutorialCanvas.enabled = false;
         journal = GameObject.Find("Journal Canvas");
         player = GameObject.Find("Player");
+        line2.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space) && line2.enabled == false)
+        {
+            line1.enabled = false;
+            line2.enabled = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && line2.enabled == true)
+        {
+            print("end of tutorial");
+            journalTutorialCanvas.enabled = false;
+            journalUI.enabled = false;
+            cutscenePlayer.PlayCutscene(idaFlower2);
+        }
     }
 
     public void OnTriggerEnter(Collider collision)
@@ -52,9 +68,9 @@ public class JournalMechanicIntro : MonoBehaviour
 
     }
 
-    public void TutorialText()
-    {
-        line1.SetActive(false);
-        line2.SetActive(true);
-    }
+    //public void TutorialText()
+    //{
+    //    line1.SetActive(false);
+    //    line2.SetActive(true);
+    //}
 }
