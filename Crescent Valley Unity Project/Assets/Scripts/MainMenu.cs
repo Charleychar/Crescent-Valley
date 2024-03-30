b1c555a540c6378508767a23f79d42bcde1dbecd
Charleychar <Charleychar@outlook.com>
2024-03-30 15:20:59 +0000

    added controls view
    option to see controls from main menu

--- Crescent Valley Unity Project/Assets/Scripts/MainMenu.cs
+++ Crescent Valley Unity Project/Assets/Scripts/MainMenu.cs
@@ -2,9 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] Canvas mainMenu;
+    [SerializeField] Canvas controls;
+
+
+    private void Start()
+    {
+        controls.enabled = false;
+    }
+
 
     public void StartGame()
     {
@@ -14,5 +25,17 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void Controls()
+    {
+        mainMenu.enabled = false;
+        controls.enabled = true;
+    }
+
+    public void BackToMenu()
+    {
+        controls.enabled = false;
+        mainMenu.enabled = true;
     }
 }
