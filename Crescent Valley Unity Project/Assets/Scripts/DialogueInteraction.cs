c9c9db415e00158a9a3d8442b0c260c9d550895e
Charleychar <Charleychar@outlook.com>
2024-04-03 12:42:09 +0100

    began working on dialogue

---
+++ Crescent Valley Unity Project/Assets/Scripts/DialogueInteraction.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueInteraction : MonoBehaviour
+{
+
+    [SerializeField] Canvas dialogueBox;
+
+    bool canInteract = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueBox.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canInteract == true && Input.GetKeyDown(KeyCode.E))
+        {
+            dialogueBox.enabled = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canInteract = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canInteract = false;
+    }
+}
