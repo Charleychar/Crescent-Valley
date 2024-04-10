28558454fcacf7b4c5fbc83647c2061968ed5ccc
Charleychar <Charleychar@outlook.com>
2024-04-10 11:33:55 +0100

    cleanup

--- Crescent Valley Unity Project/Assets/Scripts/DialogueInteraction.cs
+++ Crescent Valley Unity Project/Assets/Scripts/DialogueInteraction.cs
@@ -2,30 +2,110 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueInteraction : MonoBehaviour
 {
 
-    /*[SerializeField] Canvas dialogueBox;
+    [SerializeField] Canvas dialogueCanvas;
+    [SerializeField] GameObject dialogueBox;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogueLines;
+    public float textSpeed;
+
+    private int index;
+
 
     bool canInteract = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        dialogueBox.enabled = false;
+        dialogueCanvas.enabled = false;
+        dialogueBox.SetActive(false);
+        dialogueText.text = string.Empty;
+        //StartDialogue();
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (canInteract == true && Input.GetKeyDown(KeyCode.E))
+        if (Input.GetKeyDown(KeyCode.E) && canInteract == true)
         {
-            dialogueBox.enabled = true;
+            dialogueCanvas.enabled = true;
+            dialogueBox.SetActive(true);
+
+            StartDialogue();
+            //DisplayDialogue();
+        }
+
+        //DisplayDialogue();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dialogueText.text == dialogueLines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[index];
+            }
+        }
+
+    }
+
+    /*private void DisplayDialogue()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (dialogueText.text == dialogueLines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                dialogueText.text = dialogueLines[index];
+            }
+        }
+    }*/
+
+    private void StartDialogue()
+    {
+        index = 0;
+        dialogueText.text = string.Empty;
+        if (dialogueCanvas.enabled == true && dialogueBox.activeInHierarchy == true)
+        {
+            StartCoroutine("TypingEffect");
+        }
+        
+    }
+
+    private IEnumerator TypingEffect()
+    {
+        foreach (char c in dialogueLines[index].ToCharArray())
+        {
+            dialogueText.text += c;
+            yield return new WaitForSeconds(textSpeed);
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void NextLine()
+    {
+        if (index < dialogueLines.Length - 1)
+        {
+            index++;
+            dialogueText.text = string.Empty;
+            StartCoroutine("TypingEffect");
+        }
+        else
+        {
+            dialogueBox.SetActive(false);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         canInteract = true;
     }
@@ -33,5 +113,6 @@ public class DialogueInteraction : MonoBehaviour
     private void OnTriggerExit2D(Collider2D collision)
     {
         canInteract = false;
-    }*/
+    }
+
 }
