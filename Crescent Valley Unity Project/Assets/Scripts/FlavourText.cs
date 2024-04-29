4adbfeb9cb8731970b46dbde5b485807ac6b225a
Charleychar <Charleychar@outlook.com>
2024-04-29 21:24:12 +0100

    moved camera further away

--- Crescent Valley Unity Project/Assets/Scripts/FlavourText.cs
+++ Crescent Valley Unity Project/Assets/Scripts/FlavourText.cs
@@ -10,7 +10,7 @@ public class FlavourText : MonoBehaviour
     [SerializeField] GameObject textBox;
     [SerializeField] PlayerMovement playerMovement;
     public TextMeshProUGUI text;
-    public string textLines;
+    public string[] textLines;
     public float textSpeed;
 
     private int index;
@@ -36,8 +36,65 @@ public class FlavourText : MonoBehaviour
             textBox.SetActive(true);
 
             StartDialogue();
-            playerMovement.DisableMovement();
-            
+            playerMovement.DisableMovement();          
         }
+
+        if (Input.GetKeyDown(KeyCode.Space) && canInteract == true)
+        {
+            if (text.text == textLines[index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                text.text = textLines[index];
+                playerMovement.EnableMovement();
+            }
+        }
+    }
+
+    private IEnumerator TypingEffect()
+    {
+        foreach (char c in textLines[index].ToCharArray())
+        {
+            text.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    private void NextLine()
+    {
+        if (index < textLines.Length - 1)
+        {
+            index++;
+            text.text = string.Empty;
+            StartCoroutine("TypingEffect");
+        }
+        else
+        {
+            textBox.SetActive(false);
+        }
+    }
+
+    private void StartDialogue()
+    {
+        index = 0;
+        text.text = string.Empty;
+        if (textCanvas.enabled == true && textBox.activeInHierarchy == true)
+        {
+            StartCoroutine("TypingEffect");
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        canInteract = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        canInteract = false;
     }
 }
