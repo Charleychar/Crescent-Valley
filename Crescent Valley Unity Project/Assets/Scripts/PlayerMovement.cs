df93996a62d9aef280d5f909e75f0b349dc40830
Charleychar <Charleychar@outlook.com>
2024-05-21 21:02:47 +0100

    animations for alex are nearly there

--- Crescent Valley Unity Project/Assets/Scripts/PlayerMovement.cs
+++ Crescent Valley Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float walkSpeed = 5f;
     [SerializeField] Rigidbody2D playerRigid;
+    [SerializeField] Animator anim;
+
+    private bool facingLeft = true;
+    private Vector2 lastMoveDirection;
     private Vector2 moveInput;
     
     
@@ -17,12 +21,14 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
+        Inputs();
+        Animation();
 
-        moveInput.Normalize();
-
-        playerRigid.velocity = moveInput * walkSpeed;
+        if (moveInput.x < 0 && facingLeft == false || moveInput.x > 0 && facingLeft == true)
+        {
+            //FlipSprite();
+        }
+        
     }
 
     public void EnableMovement(string name)
@@ -32,5 +38,40 @@ public class PlayerMovement : MonoBehaviour
     public void DisableMovement()
     {
         playerRigid.Sleep();
+    }
+
+    private void Inputs()
+    {
+        float moveX = moveInput.x = Input.GetAxisRaw("Horizontal");
+        float moveY = moveInput.y = Input.GetAxisRaw("Vertical");
+
+        if ((moveX == 0 && moveY == 0) && (moveInput.x !=0 || moveInput.y != 0))
+        {
+            lastMoveDirection = moveInput;
+        }
+
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize();
+
+        playerRigid.velocity = moveInput * walkSpeed;
+    }
+
+    private void Animation()
+    {
+        anim.SetFloat("moveX", moveInput.x);
+        anim.SetFloat("moveY", moveInput.y);
+        anim.SetFloat("moveMagnitude", moveInput.magnitude);
+        anim.SetFloat("lastMoveX", lastMoveDirection.x);
+        anim.SetFloat("lastMoveY", lastMoveDirection.y);
+    }
+
+    private void FlipSprite()
+    {
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+        facingLeft = false;
     }
 }
